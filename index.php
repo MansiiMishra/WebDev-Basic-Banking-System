2051bd5da9244ec271705075758566b81a9b87a3
Manshi Mishra <69236876+MansiiMishra@users.noreply.github.com>
2021-03-21 00:29:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+
+<style>
+body {
+  font-family: "Trirong", serif;
+}
+html { 
+	background: url(bg-img.jpg) no-repeat center center fixed; 
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+  }
+
+</style>
+	<link rel="stylesheet" type="text/css" href="styleindex.css">
+</head>
+<body>
+	
+	<header>
+		<nav>
+			<h1>Manshi Mishra</h1>
+			<ul id="navli">
+				<li><a class="homered" href="index.php">Home</a></li>
+				<li><a class="homeblack" href="viewcus.php">View Customers</a></li>
+				<li><a class="homeblack" href="transaction.php">Transaction</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<div class="divider"></div>
+	<div id="divimg">
+	<style>
+      h1 {text-align: center;}
+	  h5 {text-align: center;}
+	</style>	
+		<h2><marquee width = "50%">The Spark Foundation - Web Development and Desgining Intern at Sparks Foundation (GRIP)(March 2021)</marquee></h5>
+		<h1>#Task1</h1>
+		<h1>Online Banking System</h1>
+		
+	</div>
+	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<style>
+.button {
+
+  padding: 15px 32px;
+  text-align: center;
+  font-size: 16px;
+  margin: 4px 2px;
+ 
+
+
+  
+}
+
+.button1 {
+	background-color: white;
+	position:absolute; 
+   	margin-left:30%;   
+   	width:250px;
+   	bottom:280px;
+	   box-shadow: 5px 10px;
+} 
+.button2 {
+	background-color: dark green;
+	position:absolute; 
+   	margin-left:50%;   
+   	width:300px;
+   	bottom:280px;
+	   box-shadow: 5px 10px;
+	   } 
+</style>
+</head>
+<body>
+
+<button class="button button1"> <a href = "viewcus.php" > Beneficery List</a></button>
+<button class="button button2"><a href = "transaction.php" > View Transaction History</a></button>
+
+	
+	
+
+	
+	<br>
+	<br>
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	
+		<h3>- By Manshi Mithilesh Mishra</h3>
+	
+</body>
+</html>
